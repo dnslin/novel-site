2b16adbeb6f001235eb045fa4e05f821772c5351
dnslin <i@dnsl.in>
2024-11-26 17:35:54 +0800

    feat: 去除无用依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import compressPlugin from "vite-plugin-compression";
 import removeConsole from "vite-plugin-remove-console";
-import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 export default defineConfig({
-  plugins: [vue(), compressPlugin(), removeConsole(), visualizer()],
+  plugins: [
+    vue(),
+    removeConsole({
+      includes: ["log", "warn", "error", "debug", "info"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
